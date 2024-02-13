8d338fddb2bf688829cbd8e82b2a293c43021d88
NijatS <nijatjs@code.edu.az>
2024-02-13 22:11:11 +0400

    elave olundu

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿
+#region Task1
+
+//Console.WriteLine("havanin tempraturun daxil edin");
+//int tempratur=int.Parse(Console.ReadLine());
+//if (tempratur < 15)
+//{
+//    Console.WriteLine("hava soyuqdur");
+
+//}
+//else 
+//{
+//    Console.WriteLine( "hava istidir" );
+//}
+
+#endregion
+
+#region Task2
+
+//Console.WriteLine("telebe qiymetinizi daxil edin");
+//int qiymet =int.Parse(Console.ReadLine());
+//if(qiymet >=91 &&qiymet <=100)
+//{
+//    Console.WriteLine("sizin baliniz A"  );
+
+//}
+//else  if(qiymet >=81&&qiymet <= 90)
+//{
+//    Console.WriteLine("sizin baliniz B");
+//}
+//else if (qiymet >= 71 && qiymet <= 80)
+//{
+//	Console.WriteLine("sizin baliniz C");
+//}
+//else if (qiymet >= 61 && qiymet <= 70)
+//{
+//	Console.WriteLine("sizin baliniz D");
+//}
+//else if (qiymet >= 51 && qiymet <= 60)
+//{
+//	int a = 5 + 5;
+//	Console.WriteLine("sizin baliniz E");
+//	if(a == 10 ||  a==20)
+//	{
+//        Console.WriteLine("Cavab dogrudur");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine(	"kesildiniz");
+//}
+#endregion
+
+#region Task3
+
+//Console.WriteLine("Adinizi Daxil edin");
+//string name = Console.ReadLine();
+
+//if(name == "Samir")
+//{
+//    Console.WriteLine("Siz telebesiniz");
+//}
+//else if(name == "Nicat")
+//{
+//    Console.WriteLine("Siz mentorsunuz");
+//}
+//else
+//{
+//    Console.WriteLine("Siz bura aid deyilsiniz");
+//}
+#endregion
